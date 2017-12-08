1b05baad0a815c022405c913cfd810c6569c4866
intertext.no <orstavik77@gmail.com>
2017-12-08 19:49:16 +0100

    prepping state objects using devtools.inspectedWindow.eval

--- devtools-panel.js
+++ devtools-panel.js
@@ -1,16 +1,116 @@
+const StatePrinter =
+`
+class StatePrinter {
+
+  static parse(debugInfo){
+    return {
+      task: debugInfo.task,
+      visualVersion: StatePrinter.compareObjects("state", debugInfo.startState, debugInfo.reducedState, debugInfo.newState),
+      computerInfo: StatePrinter.debug(debugInfo.computerInfo.start, debugInfo.computerInfo.stop),
+      observerInfo: StatePrinter.debug(debugInfo.observerInfo.start, debugInfo.observerInfo.stop)
+    };
+  }
+  
+  static debug(aFuncReg, bFuncReg) {
+    let C = {};
+    for (let key in aFuncReg) {
+      let A = aFuncReg[key];
+      let B = bFuncReg[key];
+      if (A === B)
+        continue;
+      let triggerPaths = [];
+      for (let i = 0; i < B.argsValue.length; i++) {
+        let a = A.argsValue[i];
+        let b = B.argsValue[i];
+        let triggered = false;
+        if (a !== b)
+          triggered = true;
+        triggerPaths[i] = {path: A.argsPaths[i], triggered: triggered};
+      }
+      C[key] = {a: A, triggerPaths: triggerPaths};
+    }
+    return C;
+  }
+
+  //.name, .diff, .style, .values.startState/.reducedState/.newState, .children
+  static compareObjects(name, startState, reducedState, newState) {
+    let res = {};
+    res.name = name;
+    res.style = [
+      "reduce" + StatePrinter.diff(startState, reducedState),
+      "compute" + StatePrinter.diff(reducedState, newState)
+    ];
+    res.values = {
+      startState: StatePrinter.getPrintValue(startState),
+      reducedState: StatePrinter.getPrintValue(reducedState),
+      newState: StatePrinter.getPrintValue(newState)
+    };
+    res.children = {};
+
+    for (let prop of StatePrinter.getAllObjectKeys(newState, reducedState, startState)) {
+      let start = startState ? startState[prop] : undefined;
+      let reduced = reducedState ? reducedState[prop] : undefined;
+      let nevv = newState ? newState[prop] : undefined;
+      res.children[prop] = StatePrinter.compareObjects(prop, start, reduced, nevv);
+    }
+    return res;
+  }
+
+  static getAllObjectKeys(computedState, reducedState, startState) {
+    let allKeys = [];
+    if (computedState && typeof computedState === "object")
+      allKeys = Object.keys(computedState);
+    if (reducedState && typeof reducedState === "object")
+      for (let key in reducedState)
+        if (allKeys.indexOf(key) === -1)
+          allKeys.push(key);
+    if (startState && typeof startState === "object")
+      for (let key in startState)
+        if (allKeys.indexOf(key) === -1)
+          allKeys.push(key);
+    return allKeys;
+  }
+
+  static getPrintValue(obj) {
+    if (!obj)
+      return obj;
+    if (typeof obj === "object")
+      return "";
+    return obj;
+  }
+
+  static diff(a, b) {
+    if (a === b) return "NoChange";
+    if (a === undefined) return "Added";
+    if (b === undefined) return "Deleted";
+    return "Altered";
+  }
+}
+`;
+
+let cb = `
+ITObservableState.debugHook = function(snap){
+  let res = StatePrinter.parse(snap); 
+  console.log("hooked: ", res); 
+  Tools.emit('state-changed-debug', JSON.stringify(res));
+};
+`;
+
 chrome.runtime.sendMessage({
   tabId: chrome.devtools.inspectedWindow.tabId,
   filename: "content-script.js"
 });
 
+chrome.devtools.inspectedWindow.eval(StatePrinter + cb);
+
+const taskListUL = document.querySelector("#taskList>ul");
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.name === 'new-client-state') {
-    const li = makeTaskLI(Tools.resurrectDebugInfo(request.payload));
+    const li = makeTaskLI(JSON.parse(request.payload));
     taskListUL.appendChild(li);
   }
 });
-
-// const mainList = [];
 
 const makeStateTreeUL = function (visualVersion) {
   let childrenText = "";
@@ -29,8 +129,7 @@ const makeStateTreeUL = function (visualVersion) {
   return res;
 };
 
-const makeFuncUL = function (info, compute) {
-  let filter = StatePrinter.debug(info.start, info.stop);
+const makeFuncUL = function (filter, isCompute) {
   let str = "";
   for (let funcName in filter) {
     let data = filter[funcName];
@@ -38,7 +137,7 @@ const makeFuncUL = function (info, compute) {
       `<span class='funcArgPath ${p.triggered ? "triggered" : ""}'>${p.path.join(".")}</span>`
     );
     let args = spannedArgs.join(", ");
-    let returnValue = compute ?
+    let returnValue = isCompute ?
       `<span class="returnProp">${data.a.returnProp}</span><span class="pointsTo"> = </span>` :
       "<span class='observeEntry'> => </span>";
     let li = `
@@ -54,7 +153,7 @@ const makeFuncUL = function (info, compute) {
 
 const makeTaskLI = function (debugInfo) {
   let task = debugInfo.task;
-  let visualVersion = StatePrinter.compareObjects("state", debugInfo.startState, debugInfo.reducedState, debugInfo.newState);
+  let visualVersion = debugInfo.visualVersion;
   let stateTreeUL = makeStateTreeUL(visualVersion);
   let addedDate = (new Date(task.added)).toISOString();
   let timeToCompute = Math.round((task.stop - task.start) * 100) / 100;
@@ -76,28 +175,3 @@ const makeTaskLI = function (debugInfo) {
   `;*/
   return li;
 };
-
-// const getDebugs =
-//   `(function(){
-//   let res = ITObservableState.debugList;
-//   ITObservableState.debugList = [];
-//   return res;
-// }).call()`;
-
-const taskListUL = document.querySelector("#taskList>ul");
-
-// setInterval(function () {
-//   chrome.devtools.inspectedWindow.eval(getDebugs, {}, function (list) {
-//     if (!list)
-//       return;
-//     for (let debugInfo of list)
-//       mainList.push(debugInfo);
-//   });
-
-//   while(mainList.length > 0) {
-//     let firstIn = mainList.shift();
-//     let li = makeTaskLI(firstIn);
-//     taskListUL.appendChild(li);
-//   }
-
-// }, 2000);
