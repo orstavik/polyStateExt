fe17baeabbc5268d1e95d14e53a743f861ec8647
enlightenmentor <fales.tom.1994@gmail.com>
2017-12-08 16:23:41 +0200

    some ui improvements

--- devtools-panel.js
+++ devtools-panel.js
@@ -13,14 +13,24 @@ chrome.runtime.sendMessage({
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.name === 'new-client-state') {
-    const li = makeTaskLI(JSON.parse(request.payload));
+    const snapShot = JSON.parse(request.payload);
+    const li = makeTaskLI(snapShot);
     taskListUL.appendChild(li);
+    rememberSnap(snapShot);
+    details.innerHTML = `<ul class="stateTree">${stateList[stateList.length-1]}</ul>`;
   }
 });
 
-// const mainList = [];
+const taskListUL = document.querySelector("#taskList>ul");
+const details = document.querySelector('#stateDetails');
+const stateList = [];
 
-const makeStateTreeUL = function (visualVersion) {
+function rememberSnap(snap) {
+  let visualVersion = StatePrinter.compareObjects("state", snap.startState, snap.reducedState, snap.newState);
+  stateList.push(makeStateTreeUL(visualVersion));
+}
+
+function makeStateTreeUL(visualVersion) {
   let childrenText = "";
   for (let childName in visualVersion.children)
     childrenText += "\n" + makeStateTreeUL(visualVersion.children[childName]);
@@ -37,7 +47,7 @@ const makeStateTreeUL = function (visualVersion) {
   return res;
 }
 
-const makeFuncUL = function (info, compute) {
+function makeFuncUL(info, compute) {
   let filter = StatePrinter.debug(info.start, info.stop);
   let str = "";
   for (let funcName in filter) {
@@ -60,52 +70,53 @@ const makeFuncUL = function (info, compute) {
   return "<ul class='listOfFuncs'>" + str + "</ul>";
 }
 
-const makeTaskLI = function (debugInfo) {
+function makeAddedTime(timestamp) {
+  const t = new Date(timestamp);
+  let h = t.getHours();
+  h = '0'.repeat(2-h.toString().length)+h;
+  let m = t.getMinutes();
+  m = '0'.repeat(2-m.toString().length)+m;
+  let s = t.getSeconds();
+  s = '0'.repeat(2-s.toString().length)+s;
+  let ms = t.getMilliseconds();
+  ms = '0'.repeat(3-ms.toString().length)+ms;
+  return `${h}:${m}:${s}.${ms}`;
+}
+
+function makeTaskLI (debugInfo) {
   let task = debugInfo.task;
-  let visualVersion = StatePrinter.compareObjects("state", debugInfo.startState, debugInfo.reducedState, debugInfo.newState);
-  let stateTreeUL = makeStateTreeUL(visualVersion);
-  let addedDate = (new Date(task.added)).toISOString();
+  let addedDate = makeAddedTime(task.added);
   let timeToCompute = Math.round((task.stop - task.start) * 100) / 100;
   let computeBody = makeFuncUL(debugInfo.computerInfo, true);
   let observeBody = makeFuncUL(debugInfo.observerInfo, false);
-
   let li = document.createElement("li");
+  li.dataset.index = taskListUL.children.length;
+  li.addEventListener('click', toggleListItem);
   li.innerHTML =
-    `<span class="eventType">${task.eventType}</span>
-<span class="pointsTo"> => </span>
-<span class="taskName">${task.taskName}</span>
-<span class="added">${addedDate}</span>
-<span class="duration">${timeToCompute}</span>
-
-<div class="computes">${computeBody}</div>
-<div class="observes">${observeBody}</div>
 `
-  /*<ul class='stateTree'>${stateTreeUL}</ul>
-  `;*/
+<div>
+<div class="eventMethod">
+  <span class="eventType">${task.eventType}</span><br>
+  <span>&#10551;</span>
+  <span class="taskName">${task.taskName}</span>
+</div>
+<div class="timings">
+  <span class="added">${addedDate}</span>
+  <span class="duration">${timeToCompute}</span>
+</div>
+</div>
+<div class="compObs">
+  <div class="computes">${computeBody}</div>
+  <div class="observes">${observeBody}</div>
+</div>`;
   return li;
 }
 
-const getDebugs =
-  `(function(){
-  let res = ITObservableState.debugList; 
-  ITObservableState.debugList = []; 
-  return res;
-}).call()`;
 
-const taskListUL = document.querySelector("#taskList>ul");
-
-// setInterval(function () {
-//   chrome.devtools.inspectedWindow.eval(getDebugs, {}, function (list) {
-//     if (!list)
-//       return;
-//     for (let debugInfo of list)
-//       mainList.push(debugInfo);
-//   });
-
-//   while(mainList.length > 0) {
-//     let firstIn = mainList.shift();
-//     let li = makeTaskLI(firstIn);
-//     taskListUL.appendChild(li);
-//   }
-
-// }, 2000);
+function toggleListItem(e) {
+  const item = e.currentTarget;
+  if (item.classList.contains('opened'))
+    return item.classList.remove('opened');
+  item.classList.add('opened');
+  details.innerHTML = `<ul class="stateTree">${stateList[item.dataset.index]}</ul>`;
+}
