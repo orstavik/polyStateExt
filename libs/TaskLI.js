47458842edc9dd56afc6c215290f74b6f03b685d
intertext.no <orstavik77@gmail.com>
2017-12-12 23:16:30 +0100

    tasklist is simplified, starting to get a clear concise view going

--- libs/TaskLI.js
+++ libs/TaskLI.js
@@ -1,16 +1,16 @@
 const taskTemplate = document.createElement("li");
 taskTemplate.innerHTML =
   `<div class="listItem">
-  <div class="openArrow"></div>
+  <!--<div class="openArrow"></div>-->
   <div class="eventMethod">
-    <span class="taskName"></span>
+    <!--<span class="taskName"></span>-->
+    <ul class="content eventInput"></ul>
   </div>
   <div class="timings">
     <span class="added"></span>
   </div>
 </div>
 <div class="compObs">
-  <ul class="content eventInput"></ul>
 </div>`;
 
 class TaskLI {
@@ -20,14 +20,14 @@ class TaskLI {
     let li = taskTemplate.cloneNode(true);
     li.id = "task_" + id;
     li.dataset.index = id;
-    li.querySelector("span.taskName").innerText = task.taskName;
+    // li.querySelector("span.taskName").innerText = task.taskName;
     li.querySelector("span.added").innerText = TaskLI.makeAddedTime(task.added, task.start, task.stop);
     li.querySelector("ul.eventInput").append(ObservableStateLI.makeEventTreeLI(task.taskName, task.event));
     li.addEventListener('mousedown', TaskLI.toggleListItem);
-    li.querySelector('div.openArrow').addEventListener('mousedown', (e) => {
-      e.stopPropagation();
-      li.classList.toggle('opened')
-    });
+    // li.querySelector('div.openArrow').addEventListener('mousedown', (e) => {
+    //   e.stopPropagation();
+    //   li.classList.toggle('opened')
+    // });
     return li;
   }
 
