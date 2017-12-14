40387dd3f6d9c32cdcc6ca62b400c32537df83d0
intertext.no <orstavik77@gmail.com>
2017-12-14 13:59:44 +0100

    hyperHTML looks real good

--- libs/TaskLI.js
+++ libs/TaskLI.js
@@ -1,12 +1,38 @@
-import {wire,bind} from "../node_modules/hyperhtml/esm/index.js";
+import {wire, bind} from "../node_modules/hyperhtml/esm/index.js";
 
-const EventDetailLITemplate = document.createElement("li");
-EventDetailLITemplate.innerHTML =
-  `<div class="propWrapper">
-  <span class="stateName"></span>:
-  <span class="valueNew"></span>
-</div>
-<ul></ul>`;
+class EventDetailLI {
+
+  static template(name, value) {
+    let contentValue = "";
+    let liClass = "eventdetail";
+    let children = [];
+    if (EventDetailLI.isIterable(value)){
+      liClass += " hasChildren";
+      children = Object.entries(value);
+    } else {
+      contentValue = value;
+    }
+    return wire()`
+<li class="${liClass}">
+  <details>
+    <summary>
+      <span class="stateName">${name}</span>:
+      <span class="valueNew">${contentValue}</span>
+    </summary>
+    <ul>${children.map(entry => EventDetailLI.makeEventTreeLI(entry[0], entry[1]))}</ul>                                                           
+  </details>
+</li>
+`;
+  }
+
+  static makeEventTreeLI(name, obj) {
+    return EventDetailLI.template(name, obj);
+  }
+
+  static isIterable(obj) {
+    return obj && typeof obj === "object" && Object.keys(obj).length !== 0;
+  }
+}
 
 export const TaskLI = class TaskLI {
 
@@ -37,7 +63,8 @@ export const TaskLI = class TaskLI {
       ${TaskLI.timeTemplate(task.added, task.start, task.stop)}
     </summary>
     <ul class="task__event">
-    
+      ${EventDetailLI.makeEventTreeLI("detail", task.event.detail)}
+      ${EventDetailLI.makeEventTreeLI("type", task.event.type)}
     </ul>
   </details>
 </li>
@@ -45,27 +72,7 @@ export const TaskLI = class TaskLI {
   }
 
   static makeTaskLI(task, id) {
-    const li = TaskLI.template("task_" + id, id, task, TaskLI.showActiveState);
-    const ul = li.querySelector("ul.task__event");
-    ul.append(TaskLI.makeEventTreeLI("detail", task.event.detail));
-    ul.append(TaskLI.makeEventTreeLI("type", task.event.type));
-    return li;
-  }
-
-  static makeEventTreeLI(name, obj) {
-    const li = EventDetailLITemplate.cloneNode(true);
-    li.querySelector("div.propWrapper").addEventListener("click", (e) => li.classList.toggle("opened"));
-    li.querySelector("span.stateName").textContent = name;
-
-    if (obj && typeof obj === "object" && Object.keys(obj).length !== 0) {
-      li.classList.add("hasChildren");
-      const childUL = li.querySelector("ul");
-      for (let childName in obj)
-        childUL.appendChild(TaskLI.makeEventTreeLI(childName, obj[childName]));
-    } else {
-      li.querySelector("span.valueNew").textContent = obj;
-    }
-    return li;
+    return TaskLI.template("task_" + id, id, task, TaskLI.showActiveState);
   }
 
   static formatNumber(n, width) {
