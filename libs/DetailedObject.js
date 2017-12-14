b38945ed40306945f235bad2cac08c3295819ea7
intertext.no <orstavik77@gmail.com>
2017-12-14 16:38:00 +0100

    hyper web recursion issue

---
+++ libs/DetailedObject.js
@@ -0,0 +1,58 @@
+import HyperHTMLElement from "../node_modules/hyperhtml-element/esm/index.js";
+
+class DetailedObject extends HyperHTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.setObj("nothingToSeeHere", null);
+  }
+
+  setObj(name, obj) {
+    this.name = name;
+    this.obj = obj;
+    this.childObjs = DetailedObject.getChildren(obj);
+  }
+
+  updateObject(name, obj) {
+    this.setObj(name, obj);
+    this.render();
+    if (this.childObjs.length > 0) {
+      let childrenDO = this.shadowRoot.querySelectorAll("detailed-object");
+      for (let i = 0; i < this.childObjs.length; i++) {
+        let childObj = this.childObjs[i];
+        childrenDO[i].updateObject(childObj[0], childObj[1]);
+      }
+    }
+  }
+
+  render() {
+    if (this.childObjs.length === 0) {
+      this.html`
+        <span class="stateName">${this.name}</span> : 
+        <span class="valueNew">${this.obj}</span>
+      `;
+    } else {
+      this.html`
+      <details>
+        <summary>
+          <span class="stateName">${this.name}</span>
+        </summary>
+        <ul>
+          ${this.childObjs.map(child => this.html`
+            <li>
+              <detailed-object class="${child[0]}">${child[1]}</detailed-object>
+            </li>
+          `)}
+        </ul>                                                           
+      </details>
+    `;
+    }
+  }
+
+  static getChildren(obj) {
+    return !obj || typeof obj !== "object" ? [] : Object.entries(obj);
+  }
+}
+
+DetailedObject.define('detailed-object');
--- libs/TaskLI.js
+++ libs/TaskLI.js
@@ -1,44 +1,49 @@
 import {wire, bind} from "../node_modules/hyperhtml/esm/index.js";
 
-class EventDetailLI {
-
-  static template(name, value) {
-    let contentValue = "";
-    let liClass = "eventdetail";
-    let children = [];
-    if (EventDetailLI.isIterable(value)) {
-      liClass += " hasChildren";
-      children = Object.entries(value);
-    } else {
-      contentValue = value;
-    }
-    return wire()`
-<li class="${liClass}">
-  <details>
-    <summary>
-      <span class="stateName">${name}</span>:
-      <span class="valueNew">${contentValue}</span>
-    </summary>
-    <ul>${children.map(entry => EventDetailLI.makeEventTreeLI(entry[0], entry[1]))}</ul>                                                           
-  </details>
-</li>
-`;
-  }
-
-  static makeEventTreeLI(name, obj) {
-    return EventDetailLI.template(name, obj);
-  }
-
-  static isIterable(obj) {
-    return obj && typeof obj === "object" && Object.keys(obj).length !== 0;
-  }
-}
-
+// class EventDetailLI {
+//
+//   static template(name, value) {
+//     let contentValue = "";
+//     let liClass = "eventdetail";
+//     let children = [];
+//     if (EventDetailLI.isIterable(value)) {
+//       liClass += " hasChildren";
+//       children = Object.entries(value);
+//     } else {
+//       contentValue = value;
+//     }
+//     return wire()`
+// <li class="${liClass}">
+//   <details>
+//     <summary>
+//       <span class="stateName">${name}</span>:
+//       <span class="valueNew">${contentValue}</span>
+//     </summary>
+//     <ul>${children.map(entry => EventDetailLI.makeEventTreeLI(entry[0], entry[1]))}</ul>
+//   </details>
+// </li>
+// `;
+//   }
+//
+//   static makeEventTreeLI(name, obj) {
+//     return EventDetailLI.template(name, obj);
+//   }
+//
+//   static isIterable(obj) {
+//     return obj && typeof obj === "object" && Object.keys(obj).length !== 0;
+//   }
+// }
+//
 export const TaskLI = class TaskLI {
 
   static get ACTIVE_CLASS() {
     return 'task--active';
   }
+
+// <ul class="task__event">
+//     ${EventDetailLI.makeEventTreeLI("detail", task.event.detail)}
+//     ${EventDetailLI.makeEventTreeLI("type", task.event.type)}
+//     </ul>
 
   static template(id, index, task, mouseListener) {
     return wire()`
@@ -48,10 +53,7 @@ export const TaskLI = class TaskLI {
       <span class="task__method">${task.taskName}</span>
       <added-duration class="task__time"></added-duration>
     </summary>
-    <ul class="task__event">
-      ${EventDetailLI.makeEventTreeLI("detail", task.event.detail)}
-      ${EventDetailLI.makeEventTreeLI("type", task.event.type)}
-    </ul>
+    <detailed-object></detailed-object>
   </details>
 </li>
 `;
@@ -60,6 +62,10 @@ export const TaskLI = class TaskLI {
   static makeTaskLI(task, id) {
     let template = TaskLI.template("task_" + id, id, task, TaskLI.showActiveState);
     template.querySelector("added-duration").updateTimes(task.added, task.start, task.stop);
+    template.querySelector("detailed-object").updateObject("banana", {
+      detail: task.event.detail,
+      type: task.event.type
+    });
     return template;
   }
 
