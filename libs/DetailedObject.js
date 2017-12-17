364d915f20e3196d25e682b70e457c070369e7fd
enlightenmentor <fales.tom.1994@gmail.com>
2017-12-17 13:45:26 +0200

    detailed object commented

--- libs/DetailedObject.js
+++ libs/DetailedObject.js
@@ -1,22 +1,51 @@
+/** @module DetailedObject */
+
 import HyperHTMLElement from "../node_modules/hyperhtml-element/esm/index.js";
 
+/**
+ * Class representing tree view of an object
+ * 
+ * @class DetailedObject
+ * @extends {HyperHTMLElement}
+ */
 export class DetailedObject extends HyperHTMLElement {
-
+  
+  /**
+   * Creates an instance of DetailedObject.
+   * @param {Object} props
+   * @param {string} props.name
+   * @param {Object} props.obj Properties of DetailsObject class
+   * @param {Object} attribs HTML attributes of <details-object> component
+   */
   constructor(props, attribs) {
     super();
     this.attachShadow({mode: 'open'});
-
     for (let key in attribs)
       this.setAttribute(key, attribs[key]);
-
-    props = Object.assign({
-      name: 'unset',
-      obj: null
-    }, props);
-
+    props = Object.assign({}, DetailedObject.initProps, props);
     this.updateProps(props);
   }
 
+  /**
+   * Returns default props
+   * 
+   * @readonly
+   * @static
+   */
+  static get initProps() {
+    return {
+      name: 'unset',
+      obj: null
+    };
+  }
+
+  /**
+   * Updates props and rerenders component
+   * 
+   * @param {Object} props
+   * @param {string} props.name
+   * @param {Object} props.obj Properties of DetailsObject class
+   */
   updateProps(props) {
     props = Object.assign({}, this._props, props);
     props.childObjs = !props.obj || typeof props.obj !== "object" ? [] : Object.entries(props.obj);
@@ -25,6 +54,9 @@ export class DetailedObject extends HyperHTMLElement {
     this.render();
   }
 
+  /**
+   * Renders html to the shadow dom of a component
+   */
   render() {
     if (this._props.childObjs.length === 0) {
       this.html `
