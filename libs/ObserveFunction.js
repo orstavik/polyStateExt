7c32db1960a4fac7fbf67a62cc57603b4db92947
intertext.no <orstavik77@gmail.com>
2017-12-18 22:39:57 +0100

    ObserveFunction new structure

--- libs/ObserveFunction.js
+++ libs/ObserveFunction.js
@@ -1,26 +1,27 @@
 import HyperHTMLElement from "../node_modules/hyperhtml-element/esm/index.js";
 
-export class ObserveFunction extends HyperHTMLElement {
+class ObserveFunction extends HyperHTMLElement {
 
-  static make(funcObj) {
-    const res = new ObserveFunction();
-    res.updateFuncObj(funcObj);
-    return res;
-  }
-
-  constructor() {
+  /**
+   * Creates an instance of a observer function description
+   * @param {ObserveFunction.Props} props Properties of class
+   * @param {Object} attribs Attributes of component
+   */
+  constructor(props, attribs) {
     super();
     this.attachShadow({mode: 'open'});
-    this.setFuncObj(null);
+    for (let key in attribs)
+      this.setAttribute(key, attribs[key]);
+    this.state = props;
+    this.render();
   }
 
-  setFuncObj(functionObj) {
-    this.funcName = functionObj ? functionObj.funcName : "unset";
-    this.funcArgs = functionObj ? functionObj.triggerPaths : [];
-  }
-
-  updateFuncObj(funcObj) {
-    this.setFuncObj(funcObj);
+  /**
+   * Call this method to update its properties and rerender its DOM node.
+   * @param {ObserveFunction.Props} props The new properties of this component
+   */
+  updateProps(props) {
+    this.state = ObserveFunction.Props.update(props, this.state);
     this.render();
   }
 
@@ -34,10 +35,10 @@ export class ObserveFunction extends HyperHTMLElement {
           color: lightgreen;
         }
       </style>
-      <span class="funcName">${this.funcName}</span>
+      <span class="funcName">${this.state.funcName}</span>
       <span class="pointsTo argsStart">(</span>
       <span class="funcArgs">
-        ${this.funcArgs.map((arg, i) => HyperHTMLElement.wire()`
+        ${(this.state.funcArgs || []).map((arg, i) => HyperHTMLElement.wire()`
           ${i !== 0 ? ", " : ""}
           <state-path triggered="${arg.triggered}">${arg.path.join(".")}</state-path>
         `)}
@@ -46,5 +47,21 @@ export class ObserveFunction extends HyperHTMLElement {
     `;
   }
 }
+
+ObserveFunction.Props = class {
+  /**
+   * @param {{funcName: string, triggerPaths: []}} functionObj representing the function being called
+   */
+  constructor(functionObj) {
+    this.funcName = functionObj ? functionObj.funcName : "unset";
+    this.funcArgs = functionObj ? functionObj.triggerPaths : [];
+  }
+
+  static update(newProps, oldProps) {
+    return newProps;
+  }
+};
+
+export default ObserveFunction;
 
 customElements.define('observe-function', ObserveFunction);
