73561c23a9be748c1b01f9824b1bb9d3dd35e9c2
intertext.no <orstavik77@gmail.com>
2017-12-18 13:17:50 +0100

    moved StateDetail to new structure

--- libs/StateDetail.js
+++ libs/StateDetail.js
@@ -2,49 +2,29 @@ import HyperHTMLElement from "../node_modules/hyperhtml-element/esm/index.js";
 import {StateTree} from "./StateTree.js";
 import {ObserveFunction} from "./ObserveFunction.js";
 
-export class StateDetail extends HyperHTMLElement {
+class StateDetail extends HyperHTMLElement {
 
   /**
-   * Adds two numbers
-   * @param {Object} props
-   * @param {Object} props.debugInfo
-   * @param {Object} props.visualVersion
-   * @param {Number} props.id
-   * @param {Object} attribs
+   * Creates an instance of TaskLI
+   * @param {StateDetail.Props} props Properties of class
+   * @param {Object} attribs Attributes of component
    */
   constructor(props, attribs) {
     super();
     this.attachShadow({mode: 'open'});
-
     for (let key in attribs)
       this.setAttribute(key, attribs[key]);
-
-    props = Object.assign({}, props);
-
-    this.updateProps(props);
+    this._props = props;
+    this.render();
     this.addEventListener("path-clicked", StateDetail.pathClicked);
   }
 
   /**
-   * Adds two numbers
-   * @param {Object} props
-   * @param {Object} props.debugInfo
-   * @param {Object} props.visualVersion
-   * @param {Number} props.id
+   * Call this method to update its properties and rerender its DOM node.
+   * @param {StateDetail.Props} props The new properties of this component
    */
   updateProps(props) {
-    props = Object.assign({}, this._props, props);
-
-    if (!props.debugInfo)
-      return;
-    props.observers = Object.values(props.debugInfo.observerInfo);
-
-    this._props = props;
-    this.render();
-  }
-
-  updateDebug(debugInfo, visualVersion, id) {
-    this.setDebug(debugInfo, visualVersion, id);
+    this._props = this._props.update(props);
     this.render();
   }
 
@@ -68,36 +48,68 @@ export class StateDetail extends HyperHTMLElement {
     }
   }
 
-  // static pathClicked (e){
-  //   alert("path clicked: " + e.detail);
-  //   let segments2 = ("state."+e.detail).split(".");
-  //   let stateTree = this.shadowRoot.querySelector("state-tree");
-  //   stateTree.flashPath(segments2);
-  //   // while (segments2.length){
-  //   //   let first = segments2.shift();
-  //   //   stateTree = stateTree.shadowRoot.querySelector("state-tree." + first);
-  //   //   if (!stateTree)
-  //   //     return alert("no such path: " + e.detail);
-  //   //   stateTree.classList.toggle("flash");
-  //   // }
-  //   // const oldFlash = document.querySelectorAll(".flash");
-  //   // for (let oldi of oldFlash)
-  //   //   oldi.classList.remove("flash");
-  //   //
-  //   // const index = e.path[5].contentID;
-  //   // let segments = e.currentTarget.textContent.split(".");
-  //   //
-  //   // for (let i = 0; i < segments.length; i++) {
-  //   //   let partialPath = segments.slice(0, segments.length - i);
-  //   //   let argPath = partialPath.join("_");
-  //   //   let detail = document.querySelector("#" + index + "_state_" + argPath);
-  //   //   detail.classList.add("opened");
-  //   // }
-  //   //
-  //   // let argPath = segments.join("_");
-  //   // let detail = document.querySelector("#s" + index + "_state_" + argPath);
-  //   // detail.classList.add("flash");
-  // };
+  /**
+   * Helper function to isolate css style
+   * @returns {HTMLStyleElement}
+   */
+  static _style() {
+    return HyperHTMLElement.wire()`
+      <style>
+      </style>
+    `;
+  }
 }
 
+StateDetail.Props = class {
+  /**
+   * @param {Object} observerInfo the whole debugInfo
+   * @param {Object} visualVersion computed merged into the state detail
+   */
+  constructor(observerInfo, visualVersion) {
+    if (!observerInfo || !visualVersion)
+      throw new Error("StateDetail must have both debugInfo and visualVersion when created");
+    this.visualVersion = visualVersion;
+    this.observers = Object.values(observerInfo);
+  }
+
+  update(newProps) {
+    return {
+      observers: Object.values(newProps.observerInfo || {}),
+      visualVersion: newProps.visualVersion
+    };
+  }
+};
+
+export default StateDetail;
 customElements.define("state-detail", StateDetail);
+
+// static pathClicked (e){
+//   alert("path clicked: " + e.detail);
+//   let segments2 = ("state."+e.detail).split(".");
+//   let stateTree = this.shadowRoot.querySelector("state-tree");
+//   stateTree.flashPath(segments2);
+//   // while (segments2.length){
+//   //   let first = segments2.shift();
+//   //   stateTree = stateTree.shadowRoot.querySelector("state-tree." + first);
+//   //   if (!stateTree)
+//   //     return alert("no such path: " + e.detail);
+//   //   stateTree.classList.toggle("flash");
+//   // }
+//   // const oldFlash = document.querySelectorAll(".flash");
+//   // for (let oldi of oldFlash)
+//   //   oldi.classList.remove("flash");
+//   //
+//   // const index = e.path[5].contentID;
+//   // let segments = e.currentTarget.textContent.split(".");
+//   //
+//   // for (let i = 0; i < segments.length; i++) {
+//   //   let partialPath = segments.slice(0, segments.length - i);
+//   //   let argPath = partialPath.join("_");
+//   //   let detail = document.querySelector("#" + index + "_state_" + argPath);
+//   //   detail.classList.add("opened");
+//   // }
+//   //
+//   // let argPath = segments.join("_");
+//   // let detail = document.querySelector("#s" + index + "_state_" + argPath);
+//   // detail.classList.add("flash");
+// };
