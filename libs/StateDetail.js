58f2b968ebe90a0fdeb90fc75b6fe96e3d5c7ce8
intertext.no <orstavik77@gmail.com>
2017-12-19 14:49:25 +0100

    ObserveFunction new structure

--- libs/StateDetail.js
+++ libs/StateDetail.js
@@ -1,6 +1,6 @@
 import HyperHTMLElement from "../node_modules/hyperhtml-element/esm/index.js";
 import StateTree from "./StateTree.js";
-import ObserveFunction from "./ObserveFunction.js";
+import {ObserveFunction} from "./ObserveFunction.js";
 
 class StateDetail extends HyperHTMLElement {
 
@@ -48,7 +48,7 @@ class StateDetail extends HyperHTMLElement {
       <h4 class="state-observer__header1">Observers</h4>
       <ul class="state-observer__observers">
         ${this.state.observers.map(observer => HyperHTMLElement.wire()`
-          ${new ObserveFunction(new ObserveFunction.Props(observer))}
+          ${ObserveFunction.makeOrUpdate(null, observer)}
         `)}
       </ul>
     `;
